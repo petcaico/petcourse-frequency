87bea428e35ab0c3f689bc03d9bd9fb19fe42d4b
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-06-20 12:13:28 -0300

    feat: add functio 'extract_date_from_name'

---
+++ assets/utils/dates.py
@@ -0,0 +1,8 @@
+import re
+
+def extract_date_from_name(file: str) -> str:
+    date: list[str] = re.findall(r'\d+', file)
+    if len(date[2]) == 2:
+        date[2] = f'20{date[2]}'
+
+    return f'{date[0]}/{date[1]}/{date[2]}'
