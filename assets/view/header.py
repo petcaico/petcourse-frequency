b14595c99dfc5f0b6d1704dd9c5c826b8a0b9101
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-06-20 12:16:41 -0300

    feat: add function of header

---
+++ assets/view/header.py
@@ -0,0 +1,16 @@
+import os
+
+def cabecalho():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+    print("╔══════════════════════════════════════════════════════╗")
+    print("║                 🗂️  GERENCIAMENTO DE ATAS             ║")
+    print("║     Processamento automático de frequência PET       ║")
+    print("╠══════════════════════════════════════════════════════╣")
+    print("║ [1] ➜  Com link de compartilhamento (Google Drive)   ║")
+    print("║ [2] ➜  Com pasta local zipada                        ║")
+    print("║ [0] ➜  Encerrar processo                             ║")
+    print("╚══════════════════════════════════════════════════════╝")
